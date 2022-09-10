81afa08617cd14a8cc77291a4f4ee34153c093d6
tahataha005 <taha.f.taha005@gmail.com>
2022-09-10 11:21:16 +0300

    Script-Get Main Items

--- javascript/java.js
+++ javascript/java.js
@@ -1,3 +1,9 @@
 window.onload = () =>{
+    const name = document.getElementById("name")
+    const predictBtn = document.getElementById("predictBtn")
+    const gender = document.getElementById("gender")
+    const age = document.getElementById("age")
+    const nationality = document.getElementById("nationality")
 
+    console.log(name,predictBtn,gender,age,nationality)
 }
